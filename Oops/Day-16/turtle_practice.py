7d10f4906b782bc03c908a5fd0b7874205e8f5a1
sarthaksharma660 <sarthaksharma600@gmail.com>
2021-04-05 23:24:47 +0530

    Practice of  packages, , attribute and method

--- Oops/Day-16/turtle_practice.py
+++ Oops/Day-16/turtle_practice.py
@@ -11,3 +11,10 @@ my_screen=Screen()
 print(my_screen.screensize())
 my_screen.exitonclick()
 
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
+print(table)
